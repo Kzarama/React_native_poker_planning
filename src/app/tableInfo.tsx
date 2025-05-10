1c34d8069fdc4d25851a357ccb489454f20b4a19
kevin zarama <kevin.zarama@pragma.com.co>
2025-05-09 21:00:35 -0500

    page of tableInfo added

---
+++ src/app/tableInfo.tsx
@@ -0,0 +1,43 @@
+import PokerIcon from '@shared/core/images/PokerIcon';
+import { useUserStore } from '@shared/core/store/useUserStore';
+import { ScreenLayout } from '@shared/ui/templates/ScreenLayout';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+
+export default function TableInfo() {
+  const tableName = useUserStore((state) => state.tableName);
+  const setTableName = useUserStore((state) => state.setTableName);
+
+  return (
+    <ScreenLayout>
+      <View className='flex-row items-center gap-3 ml-6'>
+        <PokerIcon />
+        <Text className='text-xl text-white'>Crear partida</Text>
+      </View>
+
+      <View className='flex-1 items-center justify-center gap-4'>
+        <Text className='text-white font-bold'>Nombre de la partida</Text>
+        <TextInput
+          value={tableName}
+          onChangeText={setTableName}
+          className='text-white border-2 font-bold border-[#BB65FF] w-2/4 rounded-[20] py-1 px-2'
+        />
+        <Link href={'/table'} asChild>
+          <Pressable
+            className='py-2 px-4 rounded-[100] bg-white font-bold
+          disabled:text-white disabled:bg-[#BB65FF] disabled:opacity-40'
+            disabled={!tableName}
+          >
+            <Text
+              className='text-gray-800 disabled:text-white'
+              disabled={!tableName}
+            >
+              Crear partida
+            </Text>
+          </Pressable>
+        </Link>
+      </View>
+    </ScreenLayout>
+  );
+}
