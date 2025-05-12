86083856111136cfc5efd60939f1cf469d904e7e
kevin zarama <kevin.zarama@pragma.com.co>
2025-05-11 22:06:32 -0500

    nativewind colors added

---
+++ colors.ts
@@ -0,0 +1,8 @@
+// src/styles/colors.js
+export const colors = {
+  theme_purple: '#BB65FF',
+  theme_title: '#FFFFFF',
+  theme_text: '#000000',
+  theme_disabled: '#9D9DA14D',
+  theme_dark_purple: '#1e0033',
+};
